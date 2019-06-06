8ec612dec28a557fb6e18188118af0826fd378e7
meisty <shaundixon666@gmail.com>
2019-06-06 22:54:53 +0100

    Started work on the spotify.rb file

--- spotify_api.rb
+++ spotify_api.rb
@@ -1,5 +1,6 @@
 require 'rspotify'
 require 'dotenv'
+require_relative 'test'
 
 Dotenv.load("api_keys.env")
 
@@ -8,4 +9,18 @@ RSpotify.authenticate(ENV["CLIENT_ID"], ENV["CLIENT_SECRET"])
 artist = RSpotify::Artist.search("grendel")
 
 grendel = artist.first
-puts grendel.genres
+
+playlist = RSpotify::Playlist
+#puts playlist.methods.sort
+playlist2 = playlist.find("21st2yfoqz6jzxvu7iatjxzqq", "4kalAo98Yx9F9ntHSI2iZ8")
+
+#puts grendel.genres
+=begin
+scrape = Scraper.new('http://digitalgunfire.com/', 'td')
+scrape.format_song_history
+
+artists = scrape.get_artists
+tracks = scrape.get_tracks
+
+artists.each { |item| puts item }
+=end
